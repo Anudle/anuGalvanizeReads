38c083dff6d914006f800cc69a4240a6cd6df061
Anudle <AnuShrestha007@gmail.com>
2016-09-21 17:27:22 -0600

    d

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ var knex = require('../db/knex');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('Im in slash')
-  knex('author').innerJoin('author_book', 'author_id', 'author.id').innerJoin('book','book_id', 'book.id').select('author_book.id as author_book_id','author.id as authors_id', 'book.id as books_id' ,'*').then(function(data){
+  knex('author')
+  //.innerJoin('author_book', 'author_id', 'author.id').innerJoin('book','book_id', 'book.id').select('author_book.id as author_book_id','author.id as authors_id', 'book.id as books_id' ,'*')
+  .then(function(data){
       console.log(data)
       res.render('index', { book: data });
   });
