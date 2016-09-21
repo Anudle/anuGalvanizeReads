d0cf9b3e8977ed3c0d0d8deed347e96cc4f3862f
Anudle <AnuShrestha007@gmail.com>
2016-09-21 15:39:13 -0600

    d

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
+
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var pg = require('pg');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'galvanize' });
+  knex('author').innerJoin('author_book', 'author_id', 'author.id').innerJoin('book','book_id', 'book.id').select('author_book.id as author_book_id','author.id as authors_id', 'book.id as books_id' ,'*').then(function(data){
+      res.render('index', { book: data });
+  });
 });
 
 router.get('/books', function(req, res, next) {
