68b9d4b9caa8025a5cce4583ca3bada77d25aec0
Anudle <AnuShrestha007@gmail.com>
2016-09-21 14:18:43 -0600

    added book.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+var pg = require('pg');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/books', function(req, res, next) {
+  knex('book').then(function(data){
+    res.render('books', { title: 'Books',
+                          data: data});
+   });
+})
+
+router.get('/book-data/:id', function(req, res, next) {
+  knex('book').where({id:req.params.id}).then(function(data){
+  res.render('books-data', {title: 'Books-data',
+                            data: data[0]});
+    });
+});
+
 
 module.exports = router;
