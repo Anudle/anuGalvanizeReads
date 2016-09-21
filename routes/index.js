439f76863fd3f78748dfbf701ef32507cc66e2bd
Anudle <AnuShrestha007@gmail.com>
2016-09-21 17:22:09 -0600

    d

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
-var pg = require('pg');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
