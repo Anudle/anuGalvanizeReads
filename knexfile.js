7dab6807dfab7fca54a1d1034830adfe36fc549f
Anudle <AnuShrestha007@gmail.com>
2016-09-21 15:57:00 -0600

    d

--- knexfile.js
+++ knexfile.js
@@ -6,21 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/galvanize_reads'
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  production:{
+     client: 'pg',
+     connection: process.env.DATABASE_URL + '?ssl=true'
+   }
 };
