76ac25875a2ccbfe3ac0e21f013ede27a6188111
Anudle <AnuShrestha007@gmail.com>
2016-09-22 00:47:14 -0600

    pretty book features

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/books', routes);
+app.use('/books-add', routes);
+app.use('/books/:id', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
