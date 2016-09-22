4950568c6ab86ff746f1941d51b19f54657edef2
Anudle <AnuShrestha007@gmail.com>
2016-09-21 19:26:49 -0600

    adding books features

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,15 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/books', function(req, res, next) {
   knex('author').innerJoin('author_book', 'author_id', 'author.id').innerJoin('book','book_id', 'book.id').select('author_book.id as author_book_id','author.id as authors_id', 'book.id as books_id' ,'*').then(function(data){
-      res.render('index', { book: data });
+      res.render('books', { book: data });
   });
 });
 
-router.get('/books', function(req, res, next) {
-  knex('book').then(function(data){
-    res.render('books', { title: 'Books'});
+router.get('/', function(req, res, next) {
+    res.render('books', { title: 'Galvanize Reads'});
    });
-})
 
 
 
